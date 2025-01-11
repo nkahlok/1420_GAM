744623c9ee61fb210256dcafc2ef6331c9f20690
jh213232 <87266095+jh213232@users.noreply.github.com>
2025-01-12 00:12:15 +0800

    Added Parallax scrolling + test bg
    title

---
+++ 1420_GAM_Unity/Assets/Test Assets/Parallax.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    private float startingPos; //starting position of sprites
+    private float lengthOfSprite;    //length of sprites
+    public float AmountOfParallax;  //amount of parallax scroll
+    public PlayerCam MainCamera;   //reference of the camera
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //Getting the starting X position of sprite.
+        startingPos = transform.position.x;
+        //length of sprites
+        lengthOfSprite = GetComponent<SpriteRenderer>().bounds.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 Position = MainCamera.transform.position;
+        float Temp = Position.x * (1 - AmountOfParallax);
+        float Distance = Position.x * AmountOfParallax;
+
+        Vector3 NewPosition = new Vector3(startingPos + Distance, transform.position.y, transform.position.z);
+
+        transform.position = NewPosition;
+    }
+}
