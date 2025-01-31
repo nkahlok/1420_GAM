2fbcdc39fa850817f000513ce482887d811638b4
jh213232 <87266095+jh213232@users.noreply.github.com>
2025-01-31 10:34:30 +0800

    VFX
    updated hit vfx

--- 1420_GAM_Unity/Assets/Scripts/Player/Player.cs
+++ 1420_GAM_Unity/Assets/Scripts/Player/Player.cs
@@ -26,6 +26,7 @@ public class Player : MonoBehaviour
     public GameObject caneEquipped;
     public GameObject manholeEquipped;
     public ParticleSystem jumpFeathers;
+    public ParticleSystem hitEffect;
     #endregion
 
     [Space]
