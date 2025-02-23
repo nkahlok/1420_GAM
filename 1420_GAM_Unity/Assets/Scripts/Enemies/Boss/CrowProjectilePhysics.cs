f122f395f5c02cc4ce6c2411c30eb6afa0d171df
nkahlok <ngkahlok02@gmail.com>
2025-02-23 20:20:26 +0800

    Crow proj homing
    adjusted the homing to be more forgiving

--- 1420_GAM_Unity/Assets/Scripts/Enemies/Boss/CrowProjectilePhysics.cs
+++ 1420_GAM_Unity/Assets/Scripts/Enemies/Boss/CrowProjectilePhysics.cs
@@ -12,24 +12,34 @@ public class CrowProjectilePhysics : MonoBehaviour
 
     Rigidbody2D rb;
 
+    float loadingProjTime;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         player = PlayerManager.instance.player;
         rb = GetComponent<Rigidbody2D>();
+        loadingProjTime = 0.5f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        dir = (player.transform.position - transform.position).normalized;
-        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
-        rotateToTarget = Quaternion.AngleAxis(angle, Vector3.forward);
-        transform.rotation = Quaternion.Slerp (transform.rotation, rotateToTarget, rotationSpeed * Time.deltaTime);
-            
-        rb.linearVelocity = new Vector2(dir.x*projectileSpeed, dir.y*projectileSpeed);
+        if(Vector2.Distance(player.transform.position, this.transform.position) > 2)
+        {
+            dir = (player.transform.position - transform.position).normalized;
+            float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            rotateToTarget = Quaternion.AngleAxis(angle, Vector3.forward);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotateToTarget, rotationSpeed * Time.deltaTime);
+
+        }
+
+        if (loadingProjTime < 0f)
+            rb.linearVelocity = new Vector2(dir.x * projectileSpeed, dir.y * projectileSpeed);
+        else
+            rb.linearVelocity = new Vector2(0, 0);
+
+        loadingProjTime -= Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
