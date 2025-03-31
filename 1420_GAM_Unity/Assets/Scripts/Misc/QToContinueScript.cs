87142f7dd5eb06de8f2e948bbe8da9c0bee28f8e
jh213232 <87266095+jh213232@users.noreply.github.com>
2025-03-31 19:14:59 +0800

    UI
    credits tweaks, transitions between cutscene 3 to credits implemented

--- 1420_GAM_Unity/Assets/Scripts/Misc/QToContinueScript.cs
+++ 1420_GAM_Unity/Assets/Scripts/Misc/QToContinueScript.cs
@@ -1,13 +1,19 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class QToContinueScript : MonoBehaviour
 {
     public LevelLoaderScript levelLoader;
+    public bool isCreditsScene = false;
     void Update()
     {
-        if (Input.GetKey(KeyCode.Q))
+        if (Input.GetKey(KeyCode.Q) && !isCreditsScene)
         {
             levelLoader.LoadNextLevel();
+        }
+        if (Input.GetKey(KeyCode.Q) && isCreditsScene)
+        {
+            levelLoader.LoadMenu();
         }
     }
 }
